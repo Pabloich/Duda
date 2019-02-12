1e299a9d5a5b63ca0ab0702ece704bb80373576a
Pablo <29169516+Pabloich@users.noreply.github.com>
2019-02-12 14:29:47 -0600

    Codigo de duda

---
+++ src/duda/Duda.java
@@ -0,0 +1,15 @@
+package duda;
+
+public class Duda {
+
+    public static void main(String[] args) {
+        Integer x = 4, y = 4;
+        System.out.println("Valor: 4");
+        System.out.println(x == y);
+        
+        Integer xx = 500, yy = 500;
+        System.out.println("Valor: 500");
+        System.out.println(xx == yy);
+    }
+    
+}
